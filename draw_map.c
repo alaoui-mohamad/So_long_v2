f68fe047e23a927514e7cdf802d28ffccab96a44
alaoui-mohamad <79001471+alaoui-mohamad@users.noreply.github.com>
2023-02-24 03:53:14 +0100

    almost

--- draw_map.c
+++ draw_map.c
@@ -1,59 +1,103 @@
 #include "mlx.h"
 #include "so_long.h"
 
-int draw_map(char **map)
+void move_player(t_st *b, int x, int y)
 {
-
-    void *mlx = mlx_init();
-    int square_size = 32;
-    int x;
-    int y;
-
-    y = ft_count_colums(map);
-    x = ft_strlen(map[0]);
-    void *win = mlx_new_window(mlx, x * square_size, y * square_size, "My Map");
-    int width;
-    int height;
-    void *exit = mlx_xpm_file_to_image(mlx, "assets/exit.xpm", &width, &height);
-    void *empty = mlx_xpm_file_to_image(mlx, "assets/empty.xpm", &width, &height);
-    void *col = mlx_xpm_file_to_image(mlx, "assets/col.xpm", &width, &height);
-    void *wall = mlx_xpm_file_to_image(mlx, "assets/wall.xpm", &width, &height);
-    void *player = mlx_xpm_file_to_image(mlx, "assets/player.xpm", &width, &height);
-
     int i = 0;
-    int j;
-    while (i < y)
+    int j = 0;
+    printf("x = %d, y = %d \n", x, y);
+    while (b->arr[i])
     {
-        j = 0;
-        while (j < x)
+        while (b->arr[i][j])
         {
-            if (map[i][j] == '1')
+            if (b->arr[i][j] == 'P')
             {
-                mlx_put_image_to_window(mlx, win, wall, square_size * j, square_size * i);
-            }
-            if (map[i][j] == '0')
-                mlx_put_image_to_window(mlx, win, empty, square_size * j, square_size * i);
-            if (map[i][j] == 'C')
-            {
-                mlx_put_image_to_window(mlx, win, empty, square_size * j, square_size * i);
-                mlx_put_image_to_window(mlx, win, col, square_size * j + square_size / 4, square_size * i + square_size / 4);
-            }
-            if (map[i][j] == 'E')
-            {
-                mlx_put_image_to_window(mlx, win, empty, square_size * j, square_size * i);
-                mlx_put_image_to_window(mlx, win, exit, square_size * j, square_size * i);
-            }
-            if (map[i][j] == 'P')
-            {
-                mlx_put_image_to_window(mlx, win, empty, square_size * j, square_size * i);
-                mlx_put_image_to_window(mlx, win, player, square_size * j, square_size * i);
+                b->arr[i][j] = '0';
+                b->arr[i + y][j + x] = 'P';
+                return;
             }
             j++;
         }
+        j = 0;
         i++;
     }
+}
+int read_key(int keycode, t_st *b)
+{
+    printf("b->arr[i] = %s \n", b->arr[0]);
+    if (keycode == 53)
+        exit(0);
+    if (keycode == 0)
+        move_player(b, -1, 0);
+    if (keycode == 2)
+        move_player(b, 0, -1);
+    if (keycode == 13)
+        move_player(b, 1, 0);
+    if (keycode == 1)
+        move_player(b, 0, 1);
+    mlx_clear_window(b->mlx, b->win);
+    draw_files(b);
+    return (0);
+}
+void read_files(t_st *b)
+{
+    b->exit = mlx_xpm_file_to_image(b->mlx, "assets/exit.xpm", &b->width, &b->height);
+    b->empty = mlx_xpm_file_to_image(b->mlx, "assets/empty.xpm", &b->width, &b->height);
+    b->col = mlx_xpm_file_to_image(b->mlx, "assets/col.xpm", &b->width, &b->height);
+    b->wall = mlx_xpm_file_to_image(b->mlx, "assets/wall.xpm", &b->width, &b->height);
+    b->player = mlx_xpm_file_to_image(b->mlx, "assets/player.xpm", &b->width, &b->height);
+}
+void drow_player_exit(t_st *b)
+{
+    if (b->arr[b->i][b->j] == 'E')
+    {
+        mlx_put_image_to_window(b->mlx, b->win, b->empty, SQUARE_SIZE * b->j, SQUARE_SIZE * b->i);
+        mlx_put_image_to_window(b->mlx, b->win, b->exit, SQUARE_SIZE * b->j, SQUARE_SIZE * b->i);
+    }
+    if (b->arr[b->i][b->j] == 'P')
+    {
+        mlx_put_image_to_window(b->mlx, b->win, b->empty, SQUARE_SIZE * b->j, SQUARE_SIZE * b->i);
+        mlx_put_image_to_window(b->mlx, b->win, b->player, SQUARE_SIZE * b->j, SQUARE_SIZE * b->i);
+    }
+}
 
-    mlx_loop(mlx);
+void draw_files(t_st *b)
+{
+    while (b->i < b->y)
+    {
+        b->j = 0;
+        while (b->j < b->x)
+        {
+            if (b->arr[b->i][b->j] == '1')
+                mlx_put_image_to_window(b->mlx, b->win, b->wall, SQUARE_SIZE * b->j, SQUARE_SIZE * b->i);
+            if (b->arr[b->i][b->j] == '0')
+                mlx_put_image_to_window(b->mlx, b->win, b->empty, SQUARE_SIZE * b->j, SQUARE_SIZE * b->i);
+            if (b->arr[b->i][b->j] == 'C')
+            {
+                mlx_put_image_to_window(b->mlx, b->win, b->empty, SQUARE_SIZE * b->j, SQUARE_SIZE * b->i);
+                mlx_put_image_to_window(b->mlx, b->win, b->col, SQUARE_SIZE * b->j + SQUARE_SIZE / 4, SQUARE_SIZE * b->i + SQUARE_SIZE / 4);
+            }
+            drow_player_exit(b);
+            b->j++;
+        }
+        b->i++;
+    }
+}
+int draw_map(char **map)
+{
+    t_st b;
+
+    b.arr = map;
+    b.mlx = mlx_init();
+    b.y = ft_count_colums(map);
+    b.x = ft_strlen(map[0]);
+    printf("x = %d, y = %d \n", b.x, b.y);
+    b.win = mlx_new_window(b.mlx, b.x * SQUARE_SIZE, b.y * SQUARE_SIZE, "My Map");
+    b.i = 0;
+    read_files(&b);
+    draw_files(&b);
+    mlx_hook(b.win, 2 , 0, read_key, &b);
+    mlx_loop(b.mlx);
 
     return 0;
 }
